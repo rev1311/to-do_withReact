6cc4e38733d4da12e24c68862e150f3d8765c604
Michael Partin <unmykx@gmail.com>
2020-02-06 19:17:44 -0800

    created About.js

---
+++ to-do_withreact/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Simple todo list created in React</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
